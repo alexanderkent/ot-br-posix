1ef86b2c3d4d18e84f434fe058d35271477ce0d1
Alexander Kent <kentdome@gmail.com>
2023-12-07 15:37:46 -0800

    simple uint64_fuzzer w/ FuzzedDataProvider

--- tests/fuzz/convert_openthread_uint64_fuzzer.cpp
+++ tests/fuzz/convert_openthread_uint64_fuzzer.cpp
@@ -1,22 +1,34 @@
 #include "code_utils.hpp"
+#include <fuzzer/FuzzedDataProvider.h>
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
 {
-    if (Size >= sizeof(uint64_t))
+    FuzzedDataProvider stream(Data, Size);
+
+    // Use FuzzedDataProvider to ensure that we always have enough data
+    std::vector<uint8_t> fuzzedData = stream.ConsumeBytes<uint8_t>(sizeof(uint64_t));
+
+    // Ensure that we have exactly sizeof(uint64_t) bytes
+    if (fuzzedData.size() < sizeof(uint64_t))
     {
-        uint64_t result = ConvertOpenThreadUint64(Data);
-
-        // Check: sum the bytes in Data and compare against result
-        uint64_t sum = 0;
-        for (size_t i = 0; i < sizeof(uint64_t); ++i)
-        {
-            sum += Data[i];
-        }
-
-        if (result > sum)
-        {
-            return 1;
-        }
+        return 0;
     }
+
+    uint64_t result = ConvertOpenThreadUint64(fuzzedData.data());
+
+    // Check: sum the bytes in fuzzedData and compare against result
+    uint64_t sum = 0;
+    for (size_t i = 0; i < sizeof(uint64_t); ++i)
+    {
+        sum += fuzzedData[i];
+    }
+
+    // Perform more comprehensive checks
+    if (result != sum)
+    {
+        // Potentially interesting case: result should equal sum for valid conversions
+        return 1;
+    }
+
     return 0;
 }
